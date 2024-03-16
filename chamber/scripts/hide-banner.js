f42fe733220c4db0e4d81d306b820ac1d7ab3f9a
zbsouth47 <zbsouth47@gmail.com>
2024-03-15 22:07:09 -0600

    day of week hiding

--- chamber/scripts/hide-banner.js
+++ chamber/scripts/hide-banner.js
@@ -1,6 +1,11 @@
 const banner = document.querySelector(".banner");
 const hideButton = document.querySelector("#hide");
+const dayOfWeek = new Date();
 
 hideButton.addEventListener("click", () => {
   banner.classList.add("hide");
 });
+
+if (dayOfWeek.getDay() < 1 || dayOfWeek.getDay() > 3) {
+  banner.classList.add("hide");
+}
